29e46cb606bd7109703d6c1fa05e66098198954f
Nvinside@gmail.com <Nvinside@gmail.com@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2011-04-20 18:14:26 +0000

    support for the new langpacks

--- trunk/Miranda IM - CK Release/Miranda/Plugins/PackUpdater/Src/Common.h
+++ trunk/Miranda IM - CK Release/Miranda/Plugins/PackUpdater/Src/Common.h
@@ -17,7 +17,7 @@ not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  
 */
 
-#define MIRANDA_VER    0x1000
+#define MIRANDA_VER    0x0900
 #define MIRANDA_CUSTOM_LP
 
 // Windows Header Files:
