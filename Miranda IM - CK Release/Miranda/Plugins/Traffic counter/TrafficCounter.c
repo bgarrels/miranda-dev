2ca6741bfbb78c955b83868bd6a7fb254b8e02d7
Nvinside <Nvinside@gmail.com>
2011-05-12 19:02:46 +0000

    ListView - added missing headers

--- Miranda IM - CK Release/Miranda/Plugins/Traffic counter/TrafficCounter.c
+++ Miranda IM - CK Release/Miranda/Plugins/Traffic counter/TrafficCounter.c
@@ -1699,7 +1699,7 @@ void CreateProtocolList(void)
 	{
 		ProtoList[i].name = (char*)mir_alloc(strlen(acc[i]->szModuleName) + 1);
 		strcpy(ProtoList[i].name, acc[i]->szModuleName);
-		ProtoList[i].tszAccountName = (TCHAR*)mir_alloc(_tcslen(acc[i]->tszAccountName));
+		ProtoList[i].tszAccountName = (TCHAR*)mir_alloc(sizeof(TCHAR) * (1 + _tcslen(acc[i]->tszAccountName)));
 		_tcscpy(ProtoList[i].tszAccountName, acc[i]->tszAccountName);
 		//
 		ProtoList[i].Flags = DBGetContactSettingByte(NULL, ProtoList[i].name, SETTINGS_PROTO_FLAGS, 3);
