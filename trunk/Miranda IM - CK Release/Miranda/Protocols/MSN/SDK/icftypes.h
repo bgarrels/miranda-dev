3169740824659f1eab65db015e4a1b6c46426640
Nvinside@gmail.com <Nvinside@gmail.com@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-02-01 22:29:44 +0000

    xml parser need a change, i will do that

--- trunk/Miranda IM - CK Release/Miranda/Protocols/MSN/SDK/icftypes.h
+++ trunk/Miranda IM - CK Release/Miranda/Protocols/MSN/SDK/icftypes.h
@@ -5,12 +5,12 @@
 
  /* File created by MIDL compiler version 6.00.0366 */
 /* Compiler settings for icftypes.idl:
-    Oicf, W1, Zp8, env=Win32 (32b run)
-    protocol : dce , ms_ext, c_ext, robust
-    error checks: allocation ref bounds_check enum stub_data 
-    VC __declspec() decoration level: 
-         __declspec(uuid()), __declspec(selectany), __declspec(novtable)
-         DECLSPEC_UUID(), MIDL_INTERFACE()
+	Oicf, W1, Zp8, env=Win32 (32b run)
+	protocol : dce , ms_ext, c_ext, robust
+	error checks: allocation ref bounds_check enum stub_data 
+	VC __declspec() decoration level: 
+		 __declspec(uuid()), __declspec(selectany), __declspec(novtable)
+		 DECLSPEC_UUID(), MIDL_INTERFACE()
 */
 //@@MIDL_FILE_HEADING(  )
 
@@ -56,50 +56,50 @@ void __RPC_USER MIDL_user_free( void * );
 
 typedef 
 enum NET_FW_POLICY_TYPE_
-    {	NET_FW_POLICY_GROUP	= 0,
+	{	NET_FW_POLICY_GROUP	= 0,
 	NET_FW_POLICY_LOCAL	= NET_FW_POLICY_GROUP + 1,
 	NET_FW_POLICY_EFFECTIVE	= NET_FW_POLICY_LOCAL + 1,
 	NET_FW_POLICY_TYPE_MAX	= NET_FW_POLICY_EFFECTIVE + 1
-    } 	NET_FW_POLICY_TYPE;
+	} 	NET_FW_POLICY_TYPE;
 
 typedef 
 enum NET_FW_PROFILE_TYPE_
-    {	NET_FW_PROFILE_DOMAIN	= 0,
+	{	NET_FW_PROFILE_DOMAIN	= 0,
 	NET_FW_PROFILE_STANDARD	= NET_FW_PROFILE_DOMAIN + 1,
 	NET_FW_PROFILE_CURRENT	= NET_FW_PROFILE_STANDARD + 1,
 	NET_FW_PROFILE_TYPE_MAX	= NET_FW_PROFILE_CURRENT + 1
-    } 	NET_FW_PROFILE_TYPE;
+	} 	NET_FW_PROFILE_TYPE;
 
 typedef 
 enum NET_FW_IP_VERSION_
-    {	NET_FW_IP_VERSION_V4	= 0,
+	{	NET_FW_IP_VERSION_V4	= 0,
 	NET_FW_IP_VERSION_V6	= NET_FW_IP_VERSION_V4 + 1,
 	NET_FW_IP_VERSION_ANY	= NET_FW_IP_VERSION_V6 + 1,
 	NET_FW_IP_VERSION_MAX	= NET_FW_IP_VERSION_ANY + 1
-    } 	NET_FW_IP_VERSION;
+	} 	NET_FW_IP_VERSION;
 
 typedef 
 enum NET_FW_SCOPE_
-    {	NET_FW_SCOPE_ALL	= 0,
+	{	NET_FW_SCOPE_ALL	= 0,
 	NET_FW_SCOPE_LOCAL_SUBNET	= NET_FW_SCOPE_ALL + 1,
 	NET_FW_SCOPE_CUSTOM	= NET_FW_SCOPE_LOCAL_SUBNET + 1,
 	NET_FW_SCOPE_MAX	= NET_FW_SCOPE_CUSTOM + 1
-    } 	NET_FW_SCOPE;
+	} 	NET_FW_SCOPE;
 
 typedef 
 enum NET_FW_IP_PROTOCOL_
-    {	NET_FW_IP_PROTOCOL_TCP	= 6,
+	{	NET_FW_IP_PROTOCOL_TCP	= 6,
 	NET_FW_IP_PROTOCOL_UDP	= 17
-    } 	NET_FW_IP_PROTOCOL;
+	} 	NET_FW_IP_PROTOCOL;
 
 typedef 
 enum NET_FW_SERVICE_TYPE_
-    {	NET_FW_SERVICE_FILE_AND_PRINT	= 0,
+	{	NET_FW_SERVICE_FILE_AND_PRINT	= 0,
 	NET_FW_SERVICE_UPNP	= NET_FW_SERVICE_FILE_AND_PRINT + 1,
 	NET_FW_SERVICE_REMOTE_DESKTOP	= NET_FW_SERVICE_UPNP + 1,
 	NET_FW_SERVICE_NONE	= NET_FW_SERVICE_REMOTE_DESKTOP + 1,
 	NET_FW_SERVICE_TYPE_MAX	= NET_FW_SERVICE_NONE + 1
-    } 	NET_FW_SERVICE_TYPE;
+	} 	NET_FW_SERVICE_TYPE;
 
 
 
