842dff39529c34a8083fcc022fc0b316a33733a2
Nvinside@gmail.com <Nvinside@gmail.com@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-04-03 10:22:11 +0000

    Watrack:MyShows progress (still don't work with Auth)

--- trunk/Miranda IM - CK Release/Miranda/Plugins/watrack/make.bat
+++ trunk/Miranda IM - CK Release/Miranda/Plugins/watrack/make.bat
@@ -1,15 +1,17 @@
 @echo off
+set myopts=-dMiranda
 set dprname=watrack.dpr
 
-..\delphi\brcc32.exe res\watrack.rc         -fores\watrack.res
-..\delphi\brcc32.exe lastfm\lastfm.rc       -folastfm\lastfm.res
-..\delphi\brcc32.exe players\mradio.rc      -foplayers\mradio.res
-..\delphi\brcc32.exe kolframe\frm.rc        -fokolframe\frm.res
-..\delphi\brcc32.exe popup\popup.rc         -fopopup\popup.res
-..\delphi\brcc32.exe proto\proto.rc         -foproto\proto.res
-..\delphi\brcc32.exe stat\stat.rc           -fostat\stat.res
-..\delphi\brcc32.exe status\status.rc       -fostatus\status.res
-..\delphi\brcc32.exe templates\templates.rc -fotemplates\templates.res
+..\..\Delphi\brcc32.exe res\watrack.rc         -fores\watrack.res
+..\..\Delphi\brcc32.exe lastfm\lastfm.rc       -folastfm\lastfm.res
+..\..\Delphi\brcc32.exe myshows\myshows.rc     -fomyshows\myshows.res
+..\..\Delphi\brcc32.exe players\mradio.rc      -foplayers\mradio.res
+..\..\Delphi\brcc32.exe kolframe\frm.rc        -fokolframe\frm.res
+..\..\Delphi\brcc32.exe popup\popup.rc         -fopopup\popup.res
+..\..\Delphi\brcc32.exe proto\proto.rc         -foproto\proto.res
+..\..\Delphi\brcc32.exe stat\stat.rc           -fostat\stat.res
+..\..\Delphi\brcc32.exe status\status.rc       -fostatus\status.res
+..\..\Delphi\brcc32.exe templates\templates.rc -fotemplates\templates.res
 
 if /i '%1' == 'fpc' (
   ..\FPC\bin\fpc.exe %myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
