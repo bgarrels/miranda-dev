72e28279a507c43ab79eac8ae3185ea46be66211
Nvinside@gmail.com <Nvinside@gmail.com@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-04-21 21:47:08 +0000

    keywords updated project fixes use lower resource now

--- trunk/Miranda IM - CK Release/Miranda/Protocols/AimOscar/src/aim.cpp
+++ trunk/Miranda IM - CK Release/Miranda/Protocols/AimOscar/src/aim.cpp
@@ -16,6 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #include "../aim.h"
 #include "../version.h"
 
@@ -59,7 +60,7 @@ static const PLUGININFOEX pluginInfo =
 	sizeof(PLUGININFOEX),
 	"AIM Protocol",
 	__VERSION_DWORD,
-	"Provides support for Aol. Instant Messenger (AIM) protocol",
+	"Provides support for AOL. Instant Messenger (AIM) protocol",
 	"Boris Krasnovskiy, Aaron Myles Landwehr",
 	"borkra@miranda-im.org",
 	"© 2008-2012 Boris Krasnovskiy, 2005-2006 Aaron Myles Landwehr",
--- trunk/Miranda IM - CK Release/Miranda/Protocols/AimOscar/src/file.h
+++ trunk/Miranda IM - CK Release/Miranda/Protocols/AimOscar/src/file.h
@@ -16,6 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #ifndef FILE_H
 #define FILE_H
 
