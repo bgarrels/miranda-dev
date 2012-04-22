623513950054c1b019ef74142ecbaa7718b094d4
Nvinside@gmail.com <Nvinside@gmail.com@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-04-22 16:08:17 +0000

    updated ext. sdk

--- trunk/Miranda IM - CK Release/Miranda/ExternalAPI/m_script.h
+++ trunk/Miranda IM - CK Release/Miranda/ExternalAPI/m_script.h
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 /*
 Miranda IM: the free IM client for Microsoft* Windows*
 
-Copyright 2000-2003 Miranda ICQ/IM project,
+Copyright 2000-2003 Miranda IM project,
 all portions of this codebase are copyrighted to the people
 listed in contributors.txt.
 
