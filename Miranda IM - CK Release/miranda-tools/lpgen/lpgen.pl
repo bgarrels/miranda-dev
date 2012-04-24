ed8ac0f38d004a179f0df2495ab0abbb561da882
Nvinside <Nvinside@gmail.com>
2012-04-24 22:37:00 +0000

    cleanup remove license text, because the whole project is already under gpl (google code) lpgen fix v2 copyright updates

--- Miranda IM - CK Release/miranda-tools/lpgen/lpgen.pl
+++ Miranda IM - CK Release/miranda-tools/lpgen/lpgen.pl
@@ -4,7 +4,7 @@
 #
 # Miranda IM: the free IM client for Microsoft* Windows*
 # 
-# Copyright 2000-2012 Miranda IM project,
+# Copyright 2000-2008 Miranda ICQ/IM project,
 # all portions of this codebase are copyrighted to the people
 # listed in contributors.txt.
 # 
@@ -21,7 +21,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-#
 
 use POSIX;
 use File::Find;
