2d773542ae57045d92a7746c0a7ff4c37c13cc95
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-01 19:32:09 +0000

    Facebook updated to the latest version 0.8.1.0

--- Miranda IM - CK Release/Miranda/Protocols/Facebook robyer/src/http.cpp
+++ Miranda IM - CK Release/Miranda/Protocols/Facebook robyer/src/http.cpp
@@ -1,13 +1,19 @@
 /*
-Facebook plugin for Miranda Instant Messenger
-_____________________________________________
+Facebook plugin for
+Miranda IM: the free IM client for Microsoft* Windows*
 
-Copyright © 2009-11 Michal Zelinka, 2011-2012 Robert Pösel
+Authors
+			(C) 2009-2011 Michal Zelinka
+			(C) 2011-2012 Robert Pösel
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,12 +21,18 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-File name      : $HeadURL$
-Revision       : $Revision$
-Last change by : $Author$
-Last change on : $Date$
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
 */
 
 #include "common.h"
--- Miranda IM - CK Release/Miranda/Protocols/Facebook robyer/src/proto.h
+++ Miranda IM - CK Release/Miranda/Protocols/Facebook robyer/src/proto.h
@@ -1,14 +1,19 @@
 /*
+Facebook plugin for
+Miranda IM: the free IM client for Microsoft* Windows*
 
-Facebook plugin for Miranda Instant Messenger
-_____________________________________________
+Authors
+			(C) 2009-2011 Michal Zelinka
+			(C) 2011-2012 Robert Pösel
 
-Copyright © 2009-11 Michal Zelinka, 2011-12 Robert Pösel
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,8 +21,18 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
 */
 
 #pragma once
@@ -58,7 +73,6 @@ public:
 	}
 
 	//PROTO_INTERFACE
-
 	virtual	HANDLE   __cdecl AddToList( int flags, PROTOSEARCHRESULT* psr );
 	virtual	HANDLE   __cdecl AddToListByEvent( int flags, int iContact, HANDLE hDbEvent );
 
@@ -107,7 +121,6 @@ public:
 	virtual	int       __cdecl OnEvent( PROTOEVENTTYPE iEventType, WPARAM wParam, LPARAM lParam );
 
 	////////////////////////
-
 	// Services
 	int  __cdecl GetMyAwayMsg( WPARAM, LPARAM );
 	int  __cdecl SetMyAwayMsg( WPARAM, LPARAM );
