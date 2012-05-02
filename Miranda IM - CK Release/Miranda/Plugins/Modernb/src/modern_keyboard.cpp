dda8ae98849584f001a1b2259fbc976dd0cd01b1
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-02 10:31:16 +0000

    hotkeys' translation in clist modern back again

--- Miranda IM - CK Release/Miranda/Plugins/Modernb/src/modern_keyboard.cpp
+++ Miranda IM - CK Release/Miranda/Plugins/Modernb/src/modern_keyboard.cpp
@@ -1,9 +1,9 @@
 /*
-
+Clist_modern plugin for
 Miranda IM: the free IM client for Microsoft* Windows*
 
-Copyright 2000-2008 Miranda ICQ/IM project, 
-all portions of this codebase are copyrighted to the people 
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
 listed in contributors.txt.
 
 This program is free software; you can redistribute it and/or
@@ -19,7 +19,18 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
 */
+
 #include "../hdr/modern_commonheaders.h"
 #include <m_hotkeys.h>
 
@@ -34,18 +45,17 @@ INT_PTR hkCloseMiranda(WPARAM wParam,LPARAM lParam)
 INT_PTR hkRestoreStatus(WPARAM wParam,LPARAM lParam)
 {
 	int nStatus = ModernGetSettingWord(NULL, "CList", "Status", ID_STATUS_OFFLINE);
-	CallService(MS_CLIST_SETSTATUSMODE, nStatus, 0);
+    CallService(MS_CLIST_SETSTATUSMODE, nStatus, 0);
 
 	return 0;
 }
 
 int InitSkinHotKeys(void)
 {
-	HOTKEYDESC shk;
-
 	CreateServiceFunction("CLIST/HK/CloseMiranda",hkCloseMiranda);
 	CreateServiceFunction("CLIST/HK/RestoreStatus",hkRestoreStatus);
 
+	HOTKEYDESC shk = {0};
 	shk.cbSize=sizeof(shk);
 
 	shk.pszDescription=LPGEN("Close Miranda");
