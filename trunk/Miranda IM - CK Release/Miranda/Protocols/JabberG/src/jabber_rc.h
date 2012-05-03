30b414e1af092476f8868ead4380f00a4a98145b
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-03 07:22:29 +0000

    small change property modified

--- trunk/Miranda IM - CK Release/Miranda/Protocols/JabberG/src/jabber_rc.h
+++ trunk/Miranda IM - CK Release/Miranda/Protocols/JabberG/src/jabber_rc.h
@@ -1,16 +1,23 @@
+#ifndef _JABBER_RC_H_
+#define _JABBER_RC_H_
+
 /*
+Jabber Protocol plugin for
+Miranda IM: the free IM client for Microsoft* Windows*
 
-Jabber Protocol Plugin for Miranda IM
-Copyright ( C ) 2002-04  Santithorn Bunchua
-Copyright ( C ) 2005-11  George Hazan
-Copyright ( C ) 2007     Maxim Mluhov
+Authors 
+			Copyright (C) 2002-2004  Santithorn Bunchua
+			Copyright (C) 2007     Maxim Mluhov, Victor Pavlychko, Artem Shpynov, Michael Stepura
+			Copyright (C) 2005-2012  George Hazan, Borkra
 
-XEP-0146 support for Miranda IM
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
-of the License, or ( at your option ) any later version.
+of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -21,14 +28,16 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-Revision       : $Revision$
-Last change on : $Date$
-Last change by : $Author$
+===============================================================================
 
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
 */
-
-#ifndef _JABBER_RC_H_
-#define _JABBER_RC_H_
 
 class CJabberAdhocSession;
 
--- trunk/Miranda IM - CK Release/Miranda/Protocols/JabberG/src/ui_utils.cpp
+++ trunk/Miranda IM - CK Release/Miranda/Protocols/JabberG/src/ui_utils.cpp
@@ -1,12 +1,21 @@
 /*
+Jabber Protocol plugin for
+Miranda IM: the free IM client for Microsoft* Windows*
 
-Object UI extensions
-Copyright (C) 2008  Victor Pavlychko, George Hazan
+Authors 
+			Copyright (C) 2002-2004		Santithorn Bunchua
+			Copyright (C) 2007			Maxim Mluhov, Victor Pavlychko, Artem Shpynov, Michael Stepura
+			Copyright (C) 2008-09		Dmitriy Chervov
+			Copyright (C) 2005-2012		George Hazan, Borkra
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
-of the License, or ( at your option ) any later version.
+of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,10 +26,15 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-Revision       : $Revision$
-Last change on : $Date$
-Last change by : $Author$
+===============================================================================
 
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
 */
 
 #include "../jabber.h"
