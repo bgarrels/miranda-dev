89f5b1e501313faab1e54717d4151f7bb128b613
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-03 21:17:52 +0000

    small change property modified cleanups

--- Miranda IM - CK Release/Miranda/Plugins/db3x/src/encrypt.c
+++ Miranda IM - CK Release/Miranda/Plugins/db3x/src/encrypt.c
@@ -1,9 +1,9 @@
 /*
-
+db3x databse for
 Miranda IM: the free IM client for Microsoft* Windows*
 
-Copyright 2000-2003 Miranda IM project, 
-all portions of this codebase are copyrighted to the people 
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
 listed in contributors.txt.
 
 This program is free software; you can redistribute it and/or
@@ -19,14 +19,21 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+
+===============================================================================
 */
 
 #include "commonheaders.h"
 #include "database.h"
 
 //VERY VERY VERY BASIC ENCRYPTION FUNCTION
-
-
 void Encrypt(char*msg,BOOL up)
 {
 	int i;
