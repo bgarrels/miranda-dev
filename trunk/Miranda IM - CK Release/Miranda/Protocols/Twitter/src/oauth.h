b43e6f76cbb22e5cdbdbe847cdcf66523d529f20
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-03 08:19:50 +0000

    small change property modified

--- trunk/Miranda IM - CK Release/Miranda/Protocols/Twitter/src/oauth.h
+++ trunk/Miranda IM - CK Release/Miranda/Protocols/Twitter/src/oauth.h
@@ -1,2 +1,37 @@
 #define OAUTH_CONSUMER_KEY L"AwSuQV9A7uXpat81MQB48g"
 #define OAUTH_CONSUMER_SECRET L"x8pPGCCV5wFs26euODb9gv4VQ4kiuxTp3ed2P8Of4"
+
+/*
+Twitter protocol for
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Auhor		Copyright © 2009-2012 Jim Porter
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+*/
