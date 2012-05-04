1e5f057888295dc516d31d188015a734daa910a3
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-04 07:47:35 +0000

    upstream changes synced to the latest version

--- Miranda IM - CK Release/Miranda/Plugins/Fingerprint Mod+/res/resource.h
+++ Miranda IM - CK Release/Miranda/Plugins/Fingerprint Mod+/res/resource.h
@@ -1,8 +1,38 @@
+/*
+Client icons for the AIM protocol plugin
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+*/
+
 #include "version.h"
 
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by resource.rc
 //##############################################################################################################
 //AIM
 #define IDI_AIM					10
@@ -502,6 +532,7 @@
 #define IDI_MIRANDA_YAOL				666
 #define IDI_MIRANDA_ZELEBOBA			667
 #define IDI_MIRANDA_DEVIL				668
+#define IDI_MIRANDA_LENINPACK			669
 
 //##############################################################################################################
 //QQ clients
