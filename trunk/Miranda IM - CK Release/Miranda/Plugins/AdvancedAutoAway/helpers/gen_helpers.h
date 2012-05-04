a55bd362d6ca2a8c41d6efa84c954a593830aa78
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-04 08:54:12 +0000

    upstream change

--- trunk/Miranda IM - CK Release/Miranda/Plugins/AdvancedAutoAway/helpers/gen_helpers.h
+++ trunk/Miranda IM - CK Release/Miranda/Plugins/AdvancedAutoAway/helpers/gen_helpers.h
@@ -1,23 +1,43 @@
-/*
-    Helper functions for Miranda-IM (www.miranda-im.org)
-    Copyright 2006 P. Boon
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
 #ifndef __GEN_HELPERS_H
 #define __GEN_HELPERS_H
+
+/*
+AdvancedAutoAway plugin for
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Author 
+			Copyright (C) 2003-2012 P. Boon
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+
+* Helper functions
+*/
 
 int ProtoServiceExists(const char *szModule, const char *szService);
 char *Hlp_GetProtocolNameA(const char *proto);
