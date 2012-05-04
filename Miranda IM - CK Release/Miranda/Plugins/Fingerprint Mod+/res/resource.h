ddcf5a6a6cfde96a9d4e0881191f21f020d3e2e0
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-04 07:51:00 +0000

    ... as always ... upstream changes

--- Miranda IM - CK Release/Miranda/Plugins/Fingerprint Mod+/res/resource.h
+++ Miranda IM - CK Release/Miranda/Plugins/Fingerprint Mod+/res/resource.h
@@ -1,5 +1,5 @@
 /*
-Client icons for the AIM protocol plugin
+Client icons for the GaduGadu protocol plugin
 Miranda IM: the free IM client for Microsoft* Windows*
 
 Copyright 2000-2012 Miranda IM project,
