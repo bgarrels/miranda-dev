f931096ba3465549add6896c5fc4cc22aa88459f
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-04 23:57:34 +0000

    small changes property modified cleanups
    project are broken, i know about it, but no problem to fix it if all is done

--- Miranda IM - CK Release/Miranda/Plugins/nudge/src/headers.h
+++ Miranda IM - CK Release/Miranda/Plugins/nudge/src/headers.h
@@ -1,31 +1,63 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
+/*
+Nudge plugin for 
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Authors
+			Copyright (C)	francois.mean@skynet.be
+							Sylvain.gougouzian@gmail.com
+							wishmaster51@googlemail.com
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+*/
 
 #pragma once
 
-// Modify the following defines if you have to target a platform prior to the ones specified below.
-// Refer to MSDN for the latest info on corresponding values for different platforms.
-#ifndef WINVER				// Allow use of features specific to Windows XP or later.
-#define WINVER 0x0501		// Change this to the appropriate value to target other versions of Windows.
+#ifndef WINVER
+#define WINVER 0x0501
 #endif
 
-#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
-#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
+#ifndef _WIN32_WINNT              
+#define _WIN32_WINNT 0x0501	
 #endif						
 
-#ifndef _WIN32_WINDOWS		// Allow use of features specific to Windows 98 or later.
-#define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
+#ifndef _WIN32_WINDOWS	
+#define _WIN32_WINDOWS 0x0410
 #endif
 
-#ifndef _WIN32_IE			// Allow use of features specific to IE 6.0 or later.
-#define _WIN32_IE 0x0600	// Change this to the appropriate value to target other versions of IE.
+#ifndef _WIN32_IE		
+#define _WIN32_IE 0x0600
 #endif
 
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
-// Windows Header Files:
 
+
+// Windows Header Files:
 #include <windows.h>
 #include <commctrl.h>
 #include <stdlib.h>
@@ -61,18 +93,9 @@
 #include <m_icolib.h>	
 
 
-/*
-*
-****************************/
 void InitOptions();
 void UninitOptions();
 
-/*
-*
-****************************/
 int Preview();
 
-/*
-*
-****************************/
 HANDLE Nudge_GethContact(HANDLE);
--- Miranda IM - CK Release/Miranda/Plugins/nudge/src/nudge.h
+++ Miranda IM - CK Release/Miranda/Plugins/nudge/src/nudge.h
@@ -1,6 +1,44 @@
 #ifndef NUDGE_H
 #define NUDGE_H
 
+/*
+Nudge plugin for 
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Authors
+			Copyright (C)	francois.mean@skynet.be
+							Sylvain.gougouzian@gmail.com
+							wishmaster51@googlemail.com
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+*/
+
 //	NUDGE account status flags
 #define	NUDGE_ACC_ST0	0x00000001		//Check (countdown) when Offline
 #define	NUDGE_ACC_ST1	0x00000002		//Check (countdown) when Online
