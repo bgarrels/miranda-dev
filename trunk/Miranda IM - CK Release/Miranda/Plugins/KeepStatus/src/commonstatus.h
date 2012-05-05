68811a32dd59ca716aa8d826a8e8dd6481366b7e
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-05 20:20:39 +0000

    small changes property modified cleanups

--- trunk/Miranda IM - CK Release/Miranda/Plugins/KeepStatus/src/commonstatus.h
+++ trunk/Miranda IM - CK Release/Miranda/Plugins/KeepStatus/src/commonstatus.h
@@ -1,25 +1,41 @@
-/*
-    AdvancedAutoAway Plugin for Miranda-IM (www.miranda-im.org)
-    KeepStatus Plugin for Miranda-IM (www.miranda-im.org)
-    StartupStatus Plugin for Miranda-IM (www.miranda-im.org)
-    Copyright 2003-2006 P. Boon
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
 #ifndef COMMONSTATUSHEADER
 #define COMMONSTATUSHEADER
+
+/*
+AdvancedAutoAway plugin for
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Author
+			Copyright (C) 2003-2006 P. Boon
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+*/
 
 #if defined( UNICODE ) && !defined( _UNICODE )
 	#define _UNICODE
@@ -29,7 +45,7 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 
-#define _WIN32_WINNT 0x0501
+#define _WIN32_WINNT 0x0601
 #include <windows.h>
 #include <shlobj.h>
 #include <uxtheme.h>
@@ -60,7 +76,7 @@
 	#define __PLATFORM_NAME  ""
 #endif
 
-#include "helpers/gen_helpers.h"
+#include "../Helpers/gen_helpers.h"
 
 typedef struct {
 	int cbSize;
