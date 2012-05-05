194cd6ffc7a362c7201268ab44b62bd565c724a2
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-05 00:04:52 +0000

    small changes property modified cleanups

--- trunk/Miranda IM - CK Release/Miranda/Plugins/PackUpdater/Version.h
+++ trunk/Miranda IM - CK Release/Miranda/Plugins/PackUpdater/Version.h
@@ -1,3 +1,40 @@
+/*
+PackUpdater plugin for 
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Author
+			Copyright (C)	2010-2012 Mataes
+							2007 ZERO_BiT
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+*/
+
 #define __MAJOR_VERSION				0
 #define __MINOR_VERSION				0
 #define __RELEASE_NUM				0
@@ -25,4 +62,4 @@
 #define __AUTHOR					"Mataes, ZERO_BiT"
 #define __AUTHOREMAIL				"mataes2007@gmail.com, zero-bit@mail.ru"
 #define __AUTHORWEB					"http://mataes.googlecode.com/svn/Miranda/Plugins/PackUpdater/"
-#define __COPYRIGHT					"© 2010 Mataes, 2007 ZERO_BiT"
+#define __COPYRIGHT					"© 2010-2012 Mataes, 2007 ZERO_BiT"
