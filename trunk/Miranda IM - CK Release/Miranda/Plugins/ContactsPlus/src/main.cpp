6ff02c6907fa182f4e33581054c71d591492db89
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-05 18:05:37 +0000

    langpack support added small changes property modified cleanups

--- trunk/Miranda IM - CK Release/Miranda/Plugins/ContactsPlus/src/main.cpp
+++ trunk/Miranda IM - CK Release/Miranda/Plugins/ContactsPlus/src/main.cpp
@@ -1,25 +1,39 @@
-// --------------------------------------------------------------------------
-//                Contacts+ for Miranda Instant Messenger
-//                _______________________________________
-// 
-// Copyright © 2002 Dominus Procellarum 
-// Copyright © 2004-2008 Joe Kucera
-// 
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-//
-// -----------------------------------------------------------------------------
+/*
+Contacts+ for
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Authors
+			Copyright © 2002 Dominus Procellarum 
+			Copyright © 2004-2008 Joe Kucera
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+*/
 
 #include "contacts.h"
 
@@ -49,6 +63,7 @@ HANDLE hHookPreBuildContactMenu = NULL;
 HANDLE hContactMenuItem = NULL;
 
 int g_UnicodeCore;
+int hLangpack;
 
 PLUGININFOEX pluginInfo = {
   sizeof(PLUGININFOEX),
@@ -84,7 +99,7 @@ static int HookDBEventAdded(WPARAM wParam, LPARAM lParam)
   //get event contents
   dbe.cbBlob = CallService(MS_DB_EVENT_GETBLOBSIZE, (WPARAM)hDbEvent, 0);
   if (dbe.cbBlob != -1)
-    dbe.pBlob = (PBYTE)_alloca(dbe.cbBlob);
+    dbe.pBlob = (PBYTE)_malloca(dbe.cbBlob);
   CallService(MS_DB_EVENT_GET, (WPARAM)hDbEvent, (LPARAM)&dbe);
   //play received sound
   SkinPlaySound("RecvContacts");
