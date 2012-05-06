b1d2b84bea556afe940f5ed82bd22c17acf62204
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-06 16:21:26 +0000

    small changes property modified cleanups

--- Miranda IM - CK Release/Miranda/Plugins/yapp/src/common.h
+++ Miranda IM - CK Release/Miranda/Plugins/yapp/src/common.h
@@ -1,12 +1,42 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
+/*
+YAPP for 
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Author
+            Copyright (C) 2005-2006 Scott Ellis
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+*/
 
 #pragma once
 
-// Modify the following defines if you have to target a platform prior to the ones specified below.
-// Refer to MSDN for the latest info on corresponding values for different platforms.
+
 #ifndef WINVER				// Allow use of features specific to Windows XP or later.
 #define WINVER 0x0501		// Change this to the appropriate value to target other versions of Windows.
 #endif
@@ -35,7 +65,7 @@
 #include <commctrl.h>
 #include <time.h>
 
-#define MIRANDA_VER		0x0800
+#define MIRANDA_VER    0x0A00
 
 #include <newpluginapi.h>
 #include <statusmodes.h>
--- Miranda IM - CK Release/Miranda/Plugins/yapp/src/popups2.h
+++ Miranda IM - CK Release/Miranda/Plugins/yapp/src/popups2.h
@@ -1,9 +1,47 @@
-// The following ifdef block is the standard way of creating macros which make exporting 
-// from a DLL simpler. All files within this DLL are compiled with the POPUPS2_EXPORTS
-// symbol defined on the command line. this symbol should not be defined on any project
-// that uses this DLL. This way any other project whose source files include this file see 
-// POPUPS2_API functions as being imported from a DLL, whereas this DLL sees symbols
-// defined with this macro as being exported.
+/*
+YAPP for 
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Author
+            Copyright (C) 2005-2006 Scott Ellis
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+
+* The following ifdef block is the standard way of creating macros which make exporting 
+* from a DLL simpler. All files within this DLL are compiled with the POPUPS2_EXPORTS
+* symbol defined on the command line. this symbol should not be defined on any project
+* that uses this DLL. This way any other project whose source files include this file see 
+* POPUPS2_API functions as being imported from a DLL, whereas this DLL sees symbols
+* defined with this macro as being exported.
+*/
+
+
 #ifdef POPUPS2_EXPORTS
 #define POPUPS2_API __declspec(dllexport)
 #else
