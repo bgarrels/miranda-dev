84ba0de2aa9e9942d87eeb41e85e3578a4c67d22
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-06 01:00:03 +0000

    small changes property modified cleanups

--- Miranda IM - CK Release/Miranda/Plugins/WhenWasIt/src/events.cpp
+++ Miranda IM - CK Release/Miranda/Plugins/WhenWasIt/src/events.cpp
@@ -1,7 +1,13 @@
 /*
-WhenWasIt (birthday reminder) plugin for Miranda IM
+WhenWasIt (birthday reminder) plugin for
+Miranda IM: the free IM client for Microsoft* Windows*
 
-Copyright © 2006 Cristian Libotean
+Author
+			Copyright (C) 2006 Cristian Libotean
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -16,6 +22,16 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
 */
 
 #include "events.h"
--- Miranda IM - CK Release/Miranda/Plugins/WhenWasIt/src/events.h
+++ Miranda IM - CK Release/Miranda/Plugins/WhenWasIt/src/events.h
@@ -1,7 +1,16 @@
-/*
-WhenWasIt (birthday reminder) plugin for Miranda IM
+#ifndef M_WWI_EVENTS_H
+#define M_WWI_EVENTS_H
 
-Copyright © 2006 Cristian Libotean
+/*
+WhenWasIt (birthday reminder) plugin for
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Author
+			Copyright (C) 2006 Cristian Libotean
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -16,10 +25,17 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
 
-#ifndef M_WWI_EVENTS_H
-#define M_WWI_EVENTS_H
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+*/
 
 #include "commonheaders.h"
 
