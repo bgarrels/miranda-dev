f4e4b4d6d14a3fc72ee6eafdf701f97c66a78010
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-06 18:50:10 +0000

    project update

--- Miranda IM - CK Release/Miranda/Plugins/StopSpam_Mod_SSS/resource.h
+++ Miranda IM - CK Release/Miranda/Plugins/StopSpam_Mod_SSS/resource.h
@@ -1,7 +1,36 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by stopspam.rc
-//
+/*
+Stopspam for 
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+*/
+
 #define IDD_MESSAGES                    101
 #define IDD_MAIN                        103
 #define ID_DESCRIPTION                  1001
@@ -57,7 +86,6 @@
 #define IDC_MATH_DETAILS                1031
 
 // Next default values for new objects
-// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
--- Miranda IM - CK Release/Miranda/Plugins/StopSpam_Mod_SSS/version.h
+++ Miranda IM - CK Release/Miranda/Plugins/StopSpam_Mod_SSS/version.h
@@ -1,6 +1,39 @@
 #ifndef VERSION_H_CAA3B062_9BEE_40e6_A9F9_20BBF467731B
 #define VERSION_H_CAA3B062_9BEE_40e6_A9F9_20BBF467731B
 
+/*
+Stopspam for 
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+*/
+
 #define SUBWCREV (0)
 
 #endif
