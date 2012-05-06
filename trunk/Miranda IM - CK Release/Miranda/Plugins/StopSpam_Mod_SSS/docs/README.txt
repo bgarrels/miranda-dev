a568fca7d6cd41edf1ac0b4ebe4e559d767b093d
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-06 18:50:10 +0000

    project update

---
+++ trunk/Miranda IM - CK Release/Miranda/Plugins/StopSpam_Mod_SSS/docs/README.txt
@@ -0,0 +1,5 @@
+* Build Dependencies
+    1. boost (http://www.boost.org/)
+    2. c++ compiller (because of Miranda IM have compiled with MSVC, only msvc compiled builds working fine (i have no success with mingw, please report your result :) ))
+* Runtime Dependencies
+    1. Miranda IM at least 0.8 (custom builds should be compiled fine against 0.6, 0.7 miranda api)
--- trunk/Miranda IM - CK Release/Miranda/Plugins/StopSpam_Mod_SSS/src/headers.h
+++ trunk/Miranda IM - CK Release/Miranda/Plugins/StopSpam_Mod_SSS/src/headers.h
@@ -1,3 +1,36 @@
+/*
+Stopspam for 
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+*/
+
 #include <windows.h>
 #include <stdio.h>
 #include <commctrl.h>
@@ -19,16 +52,16 @@
 #include <m_button.h>
 #include <m_contacts.h>
 
+//ExternalApi
 #include <m_variables.h>
 #include <m_folders.h>
 
-
 #include "globals.h"
-#include "stopspam.h"
+#include "../stopspam.h"
 #include "options.h"
 #include "eventhooker.h"
-#include "version.h"
-#include "resource.h"
+#include "../version.h"
+#include "../resource.h"
 #include "utilities.h"
 
 //boost
@@ -42,5 +75,5 @@
 //utf8cpp
 #include <utf8.h>
 
-
+//only for dos-plugin
 #include <m_dos.h>
