bfb0e9874e369e9347136989f59bbbb024c15aa6
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-06 05:31:28 +0000

    small changes property modified cleanups

--- trunk/Miranda IM - CK Release/Miranda/Plugins/listeningto/resource.h
+++ trunk/Miranda IM - CK Release/Miranda/Plugins/listeningto/resource.h
@@ -1,7 +1,40 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by resource.rc
-//
+/*
+ListeningTo plugin for
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Authors
+                Copyright (C) 2005-2011 Ricardo Pescuma Domenecci
+                          (C) 2010-2011 Merlin_de
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+*/
+
 #define IDD_OPTIONS                     119
 #define IDD_PLAYERS                     120
 #define IDD_FORMAT                      121
@@ -83,7 +116,6 @@
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
-// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
