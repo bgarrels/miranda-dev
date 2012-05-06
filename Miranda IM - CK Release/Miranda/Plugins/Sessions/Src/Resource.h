8e17662129c8d089a6c18c70a2e7e56d341737da
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-06 15:33:33 +0000

    small changes property modified cleanups

--- Miranda IM - CK Release/Miranda/Plugins/Sessions/Src/Resource.h
+++ Miranda IM - CK Release/Miranda/Plugins/Sessions/Src/Resource.h
@@ -1,7 +1,39 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by sessions.rc
-//
+/*
+Sessions Management plugin for
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Author
+			Copyright (C) 2007-2008 MaD_CLuSTeR
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+*/
+
 #define IDD_OPTIONS                     101
 #define IDI_SESSIONS                    102
 #define IDI_SESSIONS_LOAD               103
@@ -47,7 +79,6 @@
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
-// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        124
--- Miranda IM - CK Release/Miranda/Plugins/Sessions/Src/Utils.h
+++ Miranda IM - CK Release/Miranda/Plugins/Sessions/Src/Utils.h
@@ -1,12 +1,21 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
 /*
-Sessions Management plugin for Miranda IM
+Sessions Management plugin for
+Miranda IM: the free IM client for Microsoft* Windows*
 
-Copyright (C) 2007-2008 Danil Mozhar
+Author
+			Copyright (C) 2007-2008 MaD_CLuSTeR
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,11 +23,19 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-#ifndef __UTILS_H__
-# define __UTILS_H__
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+*/
 
 void SetInSessionOrder(HANDLE hContact,int mode,int count,unsigned int ordernum);
 void AddInSessionOrder(HANDLE hContact,int mode,int ordernum,int writemode);
