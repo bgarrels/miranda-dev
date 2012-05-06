8bf42f3213f14fa09d8001f20e8e8bc9b1ee8aac
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-06 15:43:04 +0000

    small change

--- trunk/Miranda IM - CK Release/Miranda/Plugins/mwclist/src/m_mwclc.h
+++ trunk/Miranda IM - CK Release/Miranda/Plugins/mwclist/src/m_mwclc.h
@@ -1,8 +1,12 @@
+#ifndef _M_CLC_
+#define _M_CLC_
+
 /*
+Author Artem Shpynov aka FYR
 
 Miranda IM: the free IM client for Microsoft* Windows*
 
-Copyright 2000-2003 Miranda ICQ/IM project,
+Copyright 2000-2012 Miranda IM project,
 all portions of this codebase are copyrighted to the people
 listed in contributors.txt.
 
@@ -19,9 +23,15 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
-#ifndef _M_CLC_
-#define _M_CLC_
 
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+
+===============================================================================
+*/
 
 #endif
