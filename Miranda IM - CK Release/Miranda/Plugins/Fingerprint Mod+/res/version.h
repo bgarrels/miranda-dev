b5462932486932c6a97f307cb1148e9bfd400bd1
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-06 17:45:19 +0000

    project updates

--- Miranda IM - CK Release/Miranda/Plugins/Fingerprint Mod+/res/version.h
+++ Miranda IM - CK Release/Miranda/Plugins/Fingerprint Mod+/res/version.h
@@ -1,7 +1,16 @@
 /*
-Fingerprint Mod+ (client version) icons module for Miranda IM
+Fingerprint Mod+ (client version) for
+Miranda IM: the free IM client for Microsoft* Windows*
 
-Copyright © 2006-2007 Artem Shpynov aka FYR, Bio, Faith Healer. 2009-2010 HierOS
+Authors
+Copyright (C) 2006-2007 Artem Shpynov aka FYR, Bio
+						Faith Healer
+			  2009-2010 HierOS
+			  2011-2012 Mataes
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -16,6 +25,16 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
 */
 
 #define __MAJOR_VERSION				0
