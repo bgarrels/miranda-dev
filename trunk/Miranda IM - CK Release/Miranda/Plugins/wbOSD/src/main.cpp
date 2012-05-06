9274c9937c6433f6237177f98da5b579e970d387
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-06 14:23:36 +0000

    small changes property modified cleanups

--- trunk/Miranda IM - CK Release/Miranda/Plugins/wbOSD/src/main.cpp
+++ trunk/Miranda IM - CK Release/Miranda/Plugins/wbOSD/src/main.cpp
@@ -1,10 +1,37 @@
 /*
-Wannabe OSD
-This plugin tries to become miranda's standard OSD ;-)
+Wannabe OSD plugin for
+Miranda IM: the free IM client for Microsoft* Windows*
 
-(C) 2005 Andrej Krutak
+Author
+			Copyright (C) 2005 Andrej Krutak
 
-Distributed under GNU's GPL 2 or later
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
 */
 
 #include "wbOSD.h"
@@ -85,7 +112,7 @@ static PLUGININFO pluginInfo={
 	"Show new message/status change info using onscreen display",
 	"Andrej Krutak",
 	"andree182 {at} gmail (dot) com",
-	"©2005 Andrej Krutak",
+	"©2005-2012 Andrej Krutak",
 	"http://urtax.ms.mff.cuni.cz/~andree/",
 	UNICODE_AWARE,
 	0
--- trunk/Miranda IM - CK Release/Miranda/Plugins/wbOSD/src/wbOSD.h
+++ trunk/Miranda IM - CK Release/Miranda/Plugins/wbOSD/src/wbOSD.h
@@ -1,13 +1,40 @@
 /*
-Wannabe OSD
-This plugin tries to become miranda's standard OSD ;-)
+Wannabe OSD plugin for
+Miranda IM: the free IM client for Microsoft* Windows*
 
-(C) 2005 Andrej Krutak
+Author
+			Copyright (C) 2005 Andrej Krutak
 
-Distributed under GNU's GPL 2 or later
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
 */
 
-#define _WIN32_WINNT 0x0500
+#define _WIN32_WINNT 0x0600
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <tchar.h>
