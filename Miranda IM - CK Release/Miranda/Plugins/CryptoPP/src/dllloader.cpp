10ee6a8a6bb932aab99e785a022ff9ef3506e491
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-07 07:38:27 +0000

    small changes property modified cleanups

--- Miranda IM - CK Release/Miranda/Plugins/CryptoPP/src/dllloader.cpp
+++ Miranda IM - CK Release/Miranda/Plugins/CryptoPP/src/dllloader.cpp
@@ -1,14 +1,37 @@
 /*
-* Loading dll from memory
-*
-* Written by Vitaliy Shitts (vit@shittz.ru)
-* Copyright (c) 2004 Vitaliy Shittz.
-*
-* THIS CODE IS PROVIDED "AS IS". NO WARRANTY OF ANY KIND IS EXPRESSED
-* OR IMPLIED. YOU USE AT YOUR OWN RISK. THE AUTHOR ACCEPTS NO LIABILITY 
-* IF IT CAUSES ANY DAMAGE TO YOU OR YOUR COMPUTER WHATSOEVER.
-*
-* Beware of bugs.
+Crypto++ plugin for
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Author
+Copyright (C) 2006-2009 Baloo
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
 */
 
 #include "commonheaders.h"
