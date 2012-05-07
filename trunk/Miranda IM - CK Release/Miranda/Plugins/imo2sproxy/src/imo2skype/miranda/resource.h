791efe756b973d2031f8fff7f03c1029d48dff44
Nvinside <Nvinside@e3c2807c-d160-2f44-81a9-0ee0b6e2e781>
2012-05-07 08:09:18 +0000

    cleanup

--- trunk/Miranda IM - CK Release/Miranda/Plugins/imo2sproxy/src/imo2skype/miranda/resource.h
+++ trunk/Miranda IM - CK Release/Miranda/Plugins/imo2sproxy/src/imo2skype/miranda/resource.h
@@ -1,7 +1,41 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
-// Used by res.rc
-//
+/*
+Skype lugin for 
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Author
+Copyright (C) 2009-2012 leecher
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+
+IO Layer for Internet communication using Miranda NETLIB
+*/
+
 #define IDD_OPTIONS                     101
 #define IDC_BINDPORT                    1001
 #define IDC_BINDIP                      1002
@@ -27,8 +61,7 @@
 #define IDC_STOPSKYPEPL                 1022
 #define IDC_USENETLIB                   1023
 
-// Next default values for new objects
-// 
+// Next default values for new objects 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
--- trunk/Miranda IM - CK Release/Miranda/Plugins/imo2sproxy/src/imo2skype/w32browser.c
+++ trunk/Miranda IM - CK Release/Miranda/Plugins/imo2sproxy/src/imo2skype/w32browser.c
@@ -1,9 +1,42 @@
-/* Module:  win32_browsewnd.c
-   Purpose: Displays a window with an embedded browser in order to display the Voicechat
-            flash from imo.im
-			This example is bsaed on the work of Jeff Glatt (http://www.codeproject.com/KB/COM/cwebpage.aspx?msg=2852721)
-   Author:  leecher, credits go to Jeff Glatt for his excellent work.
-   Date:    18.10.2009
+/*
+Skype lugin for 
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Author
+			Copyright (C) 2009-2012 leecher
+
+Copyright 2000-2012 Miranda IM project,
+all portions of this codebase are copyrighted to the people
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+===============================================================================
+
+File name      : $HeadURL: 
+Revision       : $Revision: 
+Last change on : $Date: 
+Last change by : $Author:
+$Id$		   : $Id$:
+
+===============================================================================
+
+* Displays a window with an embedded browser in order to display the Voicechat
+* flash from imo.im
+* This example is bsaed on the work of Jeff Glatt (http://www.codeproject.com/KB/COM/cwebpage.aspx?msg=2852721)
+* Credits go to Jeff Glatt for his excellent work.
 */
 
 #include <windows.h>
@@ -16,6 +49,7 @@
 
 #ifndef _WIN64
 #if WINVER<0x0500
+
 #define SetWindowLongPtr SetWindowLong
 #define GetWindowLongPtr GetWindowLong
 #endif
